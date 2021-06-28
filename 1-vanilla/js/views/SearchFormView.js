f0e12b402ab6422001b61f1b5e17ddba45a6d703
cmj <chmin82@gmail.com>
2021-06-28 21:25:33 +0900

    준비편-순수JS-검색폼3

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -11,20 +11,27 @@ export default class SearchFormView extends View {
     this.resetElement = qs("[type=reset]", this.element);
 
     this.showResetButton(false);
-    this.bindEvent();
+    this.bindEvents();
   }
 
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
   }
 
-  bindEvent() {
+  bindEvents() {
     on(this.inputElement, "keyup", () => this.handleKeyup());
+    on(this.element, "submit", (event) => this.handleSubmit(event));
   }
 
   handleKeyup() {
-    console.log(tag, "handleKeyup", this.inputElement.value);
     const { value } = this.inputElement;
     this.showResetButton(value.length > 0);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(tag, "handleSubmit");
+    const { value } = this.inputElement;
+    this.emit("@submit", { value })
   }
 }
