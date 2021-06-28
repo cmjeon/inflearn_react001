18f556d5748cc3c6ed576f90aa0a2bc6a7f48b0c
cmj <chmin82@gmail.com>
2021-06-28 20:53:30 +0900

    준비편-순수JS-검색폼2

--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -1,13 +1,30 @@
-import { qs } from "../helpers.js";
+import { on, qs } from "../helpers.js";
 import View from "./View.js";
+
+const tag = "[SearchFormView]";
 
 export default class SearchFormView extends View {
   constructor() {
     super(qs("#search-form-view")); // view/View.js 의 element 에 들어감
+
+    this.inputElement = qs("[type=text]", this.element);
     this.resetElement = qs("[type=reset]", this.element);
+
     this.showResetButton(false);
+    this.bindEvent();
   }
+
   showResetButton(visible = true) {
     this.resetElement.style.display = visible ? "block" : "none";
+  }
+
+  bindEvent() {
+    on(this.inputElement, "keyup", () => this.handleKeyup());
+  }
+
+  handleKeyup() {
+    console.log(tag, "handleKeyup", this.inputElement.value);
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
   }
 }
