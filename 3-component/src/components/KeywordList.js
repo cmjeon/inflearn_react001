1a5a0494f095614f3a849f67170cf646a987371d
cmj <chmin82@gmail.com>
2021-07-21 16:59:49 +0900

    `사용편2-조합:컴포넌트담기2`까지

--- 3-component/src/components/KeywordList.js
+++ 3-component/src/components/KeywordList.js
@@ -2,20 +2,35 @@ import React from 'react';
 import List from './List.js';
 import store from '../Store.js';
 
-export default class KeywordList extends List {
-  componentDidMount() {
-    const data = store.getKeywordList();
-    this.setState({
-      data,
-    })
+export default class KeywordList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      keywordList:[],
+    }
   }
 
-  renderItem(item, index) {
-    return (
-      <>
-        <span className="number">{index + 1}</span>
-        <span>{item.keyword}</span>
-      </>
+  componentDidMount() {
+    const keywordList = store.getKeywordList();
+    this.setState(
+      {keywordList}
     )
-  };
+  }
+
+  render() {
+    return (
+      <List
+        data={this.state.keywordList}
+        onClick={this.props.onClick}
+        renderItem={(item, index)=> {
+          return (
+            <>
+              <span className="number">{index+1}</span>
+              <span>{item.keyword}</span>
+            </>
+          )
+        }}
+      />
+    )
+  }
 }
